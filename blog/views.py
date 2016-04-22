667b8b3813eb2d961bf94a576bc6c6b396e82a0a
andr94 <a.luiz.souto@gmail.com>
2016-04-22 15:20:45 -0300

    Nova tela de login.

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,10 @@ from .forms import PostForm
 from django.contrib.auth.decorators import login_required
 
 @login_required
+def tela_logado(request):
+    return render(request, 'blog/tela_logado.html', {})
+
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts' : posts})
